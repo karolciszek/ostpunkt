8c39a26c6950a4a1be6cb22c51c5e569bb3ab315
Karol Ciszek <karol.j.ciszek@gmail.com>
2017-03-16 23:30:52 +0000

    Create a Point model with basic fields
    TODO: Consider renaming set_at to published

--- last_point_map/models.py
+++ last_point_map/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Point(models.Model):
+    lat = models.FloatField()
+    lng = models.FloatField()
+    set_at = models.DateTimeField(auto_now=True)
+    username = models.CharField(max_length=150, default="")
