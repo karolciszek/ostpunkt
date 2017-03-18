b26055dca7ab4ccb6f5588cdcbade530c4c3023e
Karol Ciszek <karol.j.ciszek@gmail.com>
2017-03-18 01:02:30 +0000

    Write simple view to get the most recent point
    TODO Test it.

--- last_point_map/views.py
+++ last_point_map/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Point
 
 # Create your views here.
+def get_recent_point(request):
+    return HttpResponse(Point.objects.latest())
