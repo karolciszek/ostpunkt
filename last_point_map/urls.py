70a87e43d45c5c9efe071100e83f79a73ebc4891
Karol Ciszek <karol.j.ciszek@gmail.com>
2017-03-18 15:14:04 +0000

    Add urls for views in last_point_map

--- last_point_map/urls.py
+++ last_point_map/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^submit$', views.submit_point)
+    url(r'^$', views.map_page),
+    url(r'^submit$', views.submit_point),
+    url(r'^get_recent$', views.get_recent_point)
 ]
