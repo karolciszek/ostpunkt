5ca671368f6863516b551bf67fe178e139f9b76a
Karol Ciszek <karol.j.ciszek@gmail.com>
2017-03-18 01:50:19 +0000

    Add user to setUp

--- last_point_map/tests.py
+++ last_point_map/tests.py
@@ -1,15 +1,25 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+import json
 from .models import Point
 from .views import get_recent_point
+
+User = get_user_model()
 
 # Create your tests here.
 class RecentPointTestCase(TestCase):
     def setUp(self):
+        self.u = User(username="test")
+        self.u.save()
+
         self.pt = Point(
             lat=24.4,
-            lng=2.30
+            lng=2.30,
+            author=self.u
         )
         self.pt.save()
 
     def tearDown(self):
         self.pt.delete()
+        self.u.delete()
+
