b1a03832322022dd0452c2c3ff2e193a473f3093
Karol Ciszek <karol.j.ciszek@gmail.com>
2017-03-18 01:30:25 +0000

    Serialize Point object before sending out request

--- last_point_map/views.py
+++ last_point_map/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core import serializers
 from .models import Point
 
 # Create your views here.
 def get_recent_point(request):
-    return HttpResponse(Point.objects.last())
+    return HttpResponse(serializers.serialize('json', [Point.objects.last()]))
