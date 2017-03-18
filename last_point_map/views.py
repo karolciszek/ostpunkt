1174d4d61c8b553cd702e2a956ab3f97296ea636
Karol Ciszek <karol.j.ciszek@gmail.com>
2017-03-18 14:12:12 +0000

    Add check for authentication to submit_point

--- last_point_map/views.py
+++ last_point_map/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden
 from django.core import serializers
 from .models import Point
 
 # Create your views here.
 def get_recent_point(request):
     return HttpResponse(serializers.serialize('json', [Point.objects.last()]))
+
+def submit_point(request):
+    if not request.user.is_authenticated():
+        return HttpResponseForbidden()
