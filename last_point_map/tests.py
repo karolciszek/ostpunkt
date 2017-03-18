4daed4a234e7d022b8f7a8c79d12b1519872e04d
Karol Ciszek <karol.j.ciszek@gmail.com>
2017-03-18 01:30:52 +0000

    Add base for tests for get_recent_point

--- last_point_map/tests.py
+++ last_point_map/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Point
+from .views import get_recent_point
 
 # Create your tests here.
+class RecentPointTestCase(TestCase):
+    def setUp(self):
+        self.pt = Point(
+            lat=24.4,
+            lng=2.30
+        )
+        self.pt.save()
+
+    def tearDown(self):
+        self.pt.delete()
