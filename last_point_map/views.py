6ff2826abe7a9f79a35f5652e36aa4a38d258197
Karol Ciszek <karol.j.ciszek@gmail.com>
2017-03-18 23:12:41 +0000

    Allow redirection url to be set in root settings

--- last_point_map/views.py
+++ last_point_map/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseForbidden
 from django.core import serializers
 from .models import Point
+try:
+    from django.conf.settings import MAP_UNAUTHENTICATED_REDIRECT
+except ImportError:
+    MAP_UNAUTHENTICATED_REDIRECT = 'auth_login' # from django-registration
 
 # Create your views here.
 def map_page(request):
     if not request.user.is_authenticated():
-        return redirect('auth_login')
+        return redirect(MAP_UNAUTHENTICATED_REDIRECT)
 
     return render(request, 'last_point_map/map.html')
 
