213a08893d9767a97a13d0ba68256280cfdb0705
Karol Ciszek <karol.j.ciszek@gmail.com>
2017-03-18 15:16:01 +0000

    Include registration URLs in project

--- ostpunkt/urls.py
+++ ostpunkt/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('registration.backends.simple.urls')),
     url(r'^', include('last_point_map.urls'))
 ]
