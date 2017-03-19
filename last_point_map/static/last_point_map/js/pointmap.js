7e4865c088e82fc0e3590203bbc179ec7cf6c75c
Karol Ciszek <karol.j.ciszek@gmail.com>
2017-03-19 09:36:00 +0000

    Move create_marker into inner scope, center map on startup

--- last_point_map/static/last_point_map/js/pointmap.js
+++ last_point_map/static/last_point_map/js/pointmap.js
@@ -8,22 +8,24 @@ window.addEventListener('map:init', function (e) {
     };
 
     var default_coords = [51.0, 0.0];
-    var default_zoom = 13;
-
-    var create_marker = function (latlng) {
-        point_marker = new L.Marker(latlng, marker_settings);
-    };
+    var default_zoom = 12;
 
     $.getJSON(
         '/get_recent',
         {},
         function (data) {
+            var create_marker = function (latlng) {
+                point_marker = new L.Marker(latlng, marker_settings);
+                point_marker.addTo(map);
+            };
+
             if (data && data.length > 0) {
                 create_marker([data[0].fields.lat, data[0].fields.lng]);
             } else {
                 create_marker(default_coords);
             }
-            point_marker.addTo(map);
+
+            map.setView(point_marker.getLatLng(), default_zoom);
         }
     );
 
