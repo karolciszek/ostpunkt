e446d7c4af747c52885c86ef5ab532bc512a454d
Karol Ciszek <karol.j.ciszek@gmail.com>
2017-03-19 20:41:36 +0000

    Add redirection to map_page after logging out

--- last_point_map/urls.py
+++ last_point_map/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.map_page),
+    url(r'^$', views.map_page, name="map_page"),
     url(r'^submit$', views.submit_point),
     url(r'^get_recent$', views.get_recent_point, name="get_recent_point")
 ]
