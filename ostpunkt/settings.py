b376a1fd034df3fcf7abf78c0ed42e2b69a7c080
Karol Ciszek <karol.j.ciszek@gmail.com>
2017-03-19 23:56:51 +0000

    Move static files from production to static_production

--- ostpunkt/settings.py
+++ ostpunkt/settings.py
@@ -125,8 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static') 
+    os.path.join(BASE_DIR, 'static_production/') 
 ]
 
 # django-registration settings
